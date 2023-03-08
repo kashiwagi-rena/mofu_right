9d6450a28aa5f75d6c1838812bbdc575059df309
kashiwagi-rena <azisai07@gmail.com>
2023-03-08 14:31:15 +0900

    Tailwindの各種設定（gemのインストール）

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  plugins: [
+    require("daisyui"),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
+  daisyui: {
+    themes: ["pastel"],
+  },
+}
