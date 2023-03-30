146646cc563507e64e78c08c0cb0b20c3621a3dd
kashiwagi-rena <azisai07@gmail.com>
2023-03-30 17:00:16 +0900

    userをコメントアウト #14

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  #belongs_to :user
 
   validates :name, presence: true, length: { maximum: 31 }
   validates :body, presence: true, length: { maximum: 31 }
