a37c2c721e493fab06d042191c52d29c34777e64
kashiwagi-rena <azisai07@gmail.com>
2023-04-02 13:35:41 +0900

    routes追加 #8

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/before_login'
+  get 'static_pages/after_login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root "top#first_view"
+  get '/before_login', to: 'static_pages#before_login'
+  post '/after_login', to: 'static_pages#after_login'
 
   resources :posts, only: %i[index new create]
 end
