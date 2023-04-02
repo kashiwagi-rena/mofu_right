bf0753fa5ba225a423d81afa4cc5f9526b1bd950
kashiwagi-rena <azisai07@gmail.com>
2023-04-02 13:30:57 +0900

    userのdb追加 #8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_164135) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_01_142851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_164135) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "post_image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
