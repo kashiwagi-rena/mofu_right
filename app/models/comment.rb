323760b5d1d9558d54f9311c4416be2717c9d486
kashiwagi-rena <azisai07@gmail.com>
2023-05-12 11:25:57 +0900

    文字制限を３１文字に修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :body, presence: true, length: { maximum: 65_535 }
+  validates :body, presence: true, length: { maximum: 31 }
 end
