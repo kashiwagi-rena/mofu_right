10abbd326076a8b580f4834da1086fa869cf4121
kashiwagi-rena <azisai07@gmail.com>
2023-05-24 15:38:53 +0900

    OGP完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,14 +15,14 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('logo_type.jpg'), # 配置するパスやファイル名によって変更すること
+        image: image_url('ogp.png'), # 配置するパスやファイル名によって変更すること
         local: 'ja-JP'
       },
       # Twitter用の設定を個別で設定する
       twitter: {
         card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
         site: '@mofu_is_justice', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
-        image: image_url('logo_type.jpg') # 配置するパスやファイル名によって変更すること
+        image: image_url('ogp.png') # 配置するパスやファイル名によって変更すること
       }
     }
   end
