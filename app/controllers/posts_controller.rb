c29cf62698e01f97dffebad58c4abffbb80650f1
kashiwagi-rena <azisai07@gmail.com>
2023-05-24 21:03:07 +0900

    削除できるようになりました！

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :destroy]
   #AWS Rekognition
   require 'aws-sdk-rekognition'
 
@@ -31,24 +31,24 @@ class PostsController < ApplicationController
         # Rekognitionの結果に基づいて、投稿の可否を判断する
         #binding.pry
         if response.labels.any? { |label| label.name.downcase.include?('person') }
-          flash[:alert] = '投稿が失敗しました。人間が写っています。'
           @post.destroy
           render :new
+          flash[:alert] = '投稿が失敗しました。人間が写っています。'
         elsif response.labels.any? { |label| label.name.downcase.include?('animal') }
-          flash[:alert] = 'もふもふの投稿が成功しました！'
           redirect_to posts_path
+          flash[:alert] = 'もふもふの投稿が成功しました！'
         else
           @post.destroy
-          flash[:alert] = '投稿が失敗しました。もふ以外が写っています。'
           render :new
+          flash[:alert] = 'ﾓﾌｼﾞｬﾅｲ・・・'
         end
       else
-        flash[:alert] = '投稿が失敗しました。画像を選択してください。'
         render :new
+        flash[:alert] = '投稿が失敗しました。画像を選択してください。'
       end
     else
-      flash[:alert] = '投稿が失敗しました。タイトルと本文は必須です。'
       render :new
+      flash[:alert] = '投稿が失敗しました。タイトルと本文は必須です。'
     end
   end
   
@@ -61,6 +61,13 @@ class PostsController < ApplicationController
   def greats
     @great_posts = current_user.great_posts.includes(:user).order(created_at: :desc)
   end 
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy!
+    redirect_to posts_path
+    flash[:success] = t('defaults.message.deleted', item: @post.name)
+  end
 
   private
 
