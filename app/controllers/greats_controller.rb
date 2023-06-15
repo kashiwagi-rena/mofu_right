d7f237353f137e5951b2ec692ea2c7e0116e7805
kashiwagi-rena <azisai07@gmail.com>
2023-06-16 01:22:30 +0900

    一旦、保留

--- app/controllers/greats_controller.rb
+++ app/controllers/greats_controller.rb
@@ -1,13 +1,26 @@
 class GreatsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    current_user.great(post)
-    redirect_back fallback_location: root_path, warning: 'いいねしました'
+    @post = Post.find(params[:post_id])
+    current_user.greats.create!(post_id: @post.id)
+    #redirect_back fallback_location: root_path, warning: 'いいねしました'
+
+    render turbo_stream: turbo_stream.replace(
+      'great-button',
+      partial: 'posts/great_button',
+      locals: { product: @product, liked: true },
+    )
   end
 
   def destroy
-    post = current_user.greats.find(params[:id]).post
-    current_user.ungreat(post)
-    redirect_back fallback_location: root_path, warning: 'いいねを解除しました'
+    @post = Post.find(params[:post_id])
+    greats = current_user.greats.find_by!(post_id: @post.id)
+    greats.destroy!
+    #redirect_back fallback_location: root_path, warning: 'いいねを解除しました'
+    
+    render turbo_stream: turbo_stream.replace(
+      'great-button',
+      partial: 'posts/great_button',
+      locals: { product: @product, liked: false },
+    )
   end
 end
